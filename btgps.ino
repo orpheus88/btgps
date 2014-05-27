54fa930457e8889c13f4f7fe4d30ba4066f4f258
orpheus88 <marcusbroemsser@gmail.com>
2014-05-27 14:14:17 +0200

    Freematics V2 BT GPS
    Slimmed Firmware to use the Freematics V2 as a Bluetooth GPS Provider,
    tested with the android app
    https://play.google.com/store/apps/details?id=googoo.android.btgps

---
+++ btgps.ino
@@ -0,0 +1,110 @@
+/*************************************************************************
+* Arduino OBD-II/G-Force Data Logger and Transmitter
+* Distributed under GPL v2.0
+* Copyright (c) 2013-14 Stanley Huang <stanleyhuangyc@gmail.com>
+* All rights reserved.
+*************************************************************************/
+
+#include <Arduino.h>
+#include <SD.h>
+#include <Wire.h>
+#include <SPI.h>
+#include "config.h"
+#if USE_SOFTSERIAL
+#include <SoftwareSerial.h>
+#endif
+#include "btgps.h"
+
+// logger states
+#define STATE_GPS_FOUND 0x4
+#define STATE_GPS_READY 0x8
+#define STATE_SLEEPING 0x20
+
+#define PMTK_SET_NMEA_UPDATE_1HZ  "$PMTK220,1000*1F\r"
+#define PMTK_SET_NMEA_UPDATE_5HZ  "$PMTK220,200*2C\r"
+#define PMTK_SET_NMEA_UPDATE_10HZ "$PMTK220,100*2F\r"
+
+class COBDLogger : public COBD, public CDataLogger
+{
+public:
+    COBDLogger():state(0) {}
+    void setup()
+    {
+        state = 0;
+#if USE_GPS
+        if (initGPS()) {
+            state |= STATE_GPS_FOUND;
+        }
+#endif
+    }
+#if USE_GPS
+    void logGPSData()
+    {
+        char lastChar;
+        // issue the command to get NMEA data (one line per request)
+        write("ATGRR\r");
+        dataTime = millis();
+        for (;;) {
+            if (available()) {
+                char c = read();
+                if (c == '>') {
+                    // prompt char received
+                    break;
+                } else {
+                    logData(c);
+                    lastChar = c;
+                }
+            } else if (millis() - dataTime > 100) {logData('t');
+                // timeout
+                break;
+            }
+        }
+#if LOG_GPS_NMEA_DATA
+        if (lastChar != '\r') {
+            logData('\r');
+        }
+#endif
+    }
+#endif
+
+#if USE_GPS
+    bool initGPS()
+    {
+     //   char buf[OBD_RECV_BUF_SIZE];
+        // setting GPS baudrate
+        write("ATBR2 38400\r");
+        receive();
+        return true;
+    }
+#endif
+    byte state;
+private:
+
+};
+
+static COBDLogger logger;
+
+void setup()
+{
+    logger.begin();
+    logger.initSender();
+    logger.setup();
+}
+
+void loop()
+{
+    uint32_t t = millis();
+#if USE_GPS
+    if (logger.state & STATE_GPS_FOUND) {
+        logger.logGPSData();
+    }
+#endif
+
+#if LOOP_INTERVAL
+    // get time elapsed
+    t = millis() - t;
+    if (t < LOOP_INTERVAL) {
+        delay(LOOP_INTERVAL - t);
+    }
+#endif
+}
---
+++ config.h
@@ -0,0 +1,53 @@
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+
+/**************************************
+* Data logging/streaming out
+**************************************/
+
+// enable(1)/disable(0) data logging (if SD card is present)
+#define ENABLE_DATA_LOG 0
+
+// enable(1)/disable(0) data streaming
+#define ENABLE_DATA_OUT 1
+
+// uses software(1)/hardware(0) serial for data streaming
+#define USE_SOFTSERIAL 1
+
+// this defines the format of data streaming
+// FORMAT_BIN is required by Freematics OBD iOS App
+#define STREAM_FORMAT FORMAT_CSV
+
+/* Default streaming baudrates:
+   9600bps for BLE
+   38400bps for BT 2.1
+*/
+#define STREAM_BAUDRATE 38400
+
+// outputs debug information
+#define VERBOSE 0
+
+/**************************************
+* Choose SD pin here
+**************************************/
+//#define SD_CS_PIN SS // generic
+//#define SD_CS_PIN 4 // ethernet shield
+//#define SD_CS_PIN 7 // microduino
+#define SD_CS_PIN 10 // SD breakout
+
+/**************************************
+* Other options
+**************************************/
+// minimum time for a loop (set in case OBD-II polling is too fast)
+#define LOOP_INTERVAL 0 /* ms */
+
+
+// enable(1)/disable(0) accelerometer/gyro
+#define USE_ACCEL 0
+
+// enable(1)/disable(0) GPS module
+#define USE_GPS 1
+#define LOG_GPS_NMEA_DATA 1
+#define LOG_GPS_PARSED_DATA 0
+
+#endif // CONFIG_H_INCLUDED
